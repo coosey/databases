adf43d49d863e61a4a08e4f3c752c9ebeadbbbc5
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-10-14 10:56:43 -0700

    refactored our schema with 2 tables

--- server/schema.sql
+++ server/schema.sql
@@ -3,18 +3,21 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages(
-  id INT,
-  username INT,
-  msg INT,
-  roomname INT,
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(40) NOT NULL,
+  message VARCHAR(255) NOT NULL,
+  roomname VARCHAR(255) NOT NULL,
   PRIMARY KEY(id)
 );
 
+CREATE TABLE users(
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(40) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+
 /* Create other tables and define schemas for them here! */
-
-
-
-
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
